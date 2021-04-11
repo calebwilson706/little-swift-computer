2ae4922c5b73767bd0f339284ad95105d9924e81
Caleb Wilson <calebwilson706@gmail.com>
2021-04-11 18:40:51 +0100

    ability to cemment out lines

--- LittleSwiftComputer/Sources/Utilities/Extensions/String-Extensions/string-to-enumerated-lines.swift
+++ LittleSwiftComputer/Sources/Utilities/Extensions/String-Extensions/string-to-enumerated-lines.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension String {
     func convertToEnumeratedArrayOfLinesRemoveBlanks() -> Array<(offset: Int, element: String)> {
-        return convertToEnumeratedArrayOfLines().filter { $0.element != "" }
+        return convertToEnumeratedArrayOfLines().filter { $0.element != "" && $0.element.first != "#" }
     }
     func convertToEnumeratedArrayOfLines() -> Array<(offset: Int, element: String)> {
         return Array(self.components(separatedBy: "\n").enumerated())
